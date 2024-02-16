dad2c732bb587890607f40f7513993ebe02e0cfc
deco-L <scotchwitchbroom@gmail.com>
2024-02-16 15:03:01 +0900

    V V

--- ex01/includes/PhoneBook.hpp
+++ ex01/includes/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/02/15 18:04:00 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/02/16 15:00:36 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 
 class	PhoneBook: Contact{
 	private:
-		static int		count;
-		static Contact	emptycontact;
+		int				count;
+		Contact			emptycontact;
 		Contact 		contacts[MAX_CONTACT];
 
 	public:
-		PhoneBook() {}
+		PhoneBook(){}
 
 		bool	addContact(const Contact &contact){
 			contacts[count] = contact;
--- ex01/srcs/phonebook.cpp
+++ ex01/srcs/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/02/15 18:16:38 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/02/16 15:02:07 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,32 @@
 int	main(void)
 {
 	PhoneBook	phonebook;
+	Contact		contact;
+	Contact		tmp;
 	std::string	readcommand;
 
 	while (true)
 	{
 		std::cin >> readcommand;
-		if (readcommand == ADD)
+		if (readcommand == ADD){
 			std::cout << "add" << std::endl;
-		else if (readcommand == SEARCH)
+			std::cin >> contact.info.family_name;
+			std::cin >> contact.info.first_name;
+			std::cin >> contact.info.nickname;
+			std::cin >> contact.info.phonenumber;
+			std::cin >> contact.info.dark_secret;
+			phonebook.addContact(contact);
+		}
+		else if (readcommand == SEARCH){
 			std::cout << "search" << std::endl;
-		else if (readcommand == EXIT)
-		{
+			tmp = phonebook.getContact(0);
+			std::cout << tmp.info.family_name;
+			std::cout << tmp.info.first_name;
+			std::cout << tmp.info.nickname;
+			std::cout << tmp.info.phonenumber;
+			std::cout << tmp.info.dark_secret;
+		}
+		else if (readcommand == EXIT){
 			std::cout << "exit" << std::endl;
 			break ;
 		}
